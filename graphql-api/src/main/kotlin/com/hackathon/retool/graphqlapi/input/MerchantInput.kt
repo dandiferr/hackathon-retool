5c785503d01bea0b8e9ff3ad02dd38d4d20af125
Andrew Feng <andrew.feng@affirm.com>
2021-01-08 11:22:49 -0600

    Update merchant

---
+++ graphql-api/src/main/kotlin/com/hackathon/retool/graphqlapi/input/MerchantInput.kt
@@ -0,0 +1,14 @@
+package com.hackathon.retool.graphqlapi.input
+
+data class MerchantInput(
+    val merchantName: String,
+    val website: String,
+    val affirmTxnFee: Double,
+    val integrationType: String,
+    val adminName: String,
+    val adminEmail: String,
+    val adminPhone: String,
+    val fraudName: String,
+    val fraudEmail: String,
+    val fraudPhone: String,
+)
--- graphql-api/src/main/kotlin/com/hackathon/retool/graphqlapi/resolvers/MerchantMutationResolver.kt
+++ graphql-api/src/main/kotlin/com/hackathon/retool/graphqlapi/resolvers/MerchantMutationResolver.kt
@@ -3,6 +3,7 @@ package com.hackathon.retool.graphqlapi.resolvers
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
 import com.hackathon.retool.graphqlapi.db.models.Merchant
 import com.hackathon.retool.graphqlapi.db.repository.MerchantsRepository
+import com.hackathon.retool.graphqlapi.input.MerchantInput
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
@@ -12,6 +13,22 @@ class MerchantMutationResolver @Autowired constructor(val repository: MerchantsR
         val ari = String.format("MERCHANT-%04d", (0..10000).random())
         val merchant = Merchant()
         merchant.ari = ari
+        repository.save(merchant)
+        return merchant
+    }
+
+    fun updateMerchant(ari: String, input: MerchantInput) : Merchant? {
+        val merchant: Merchant = repository.findByAri(ari) ?: return null
+        merchant.merchantName = input.merchantName
+        merchant.website = input.website
+        merchant.affirmTxnFee = input.affirmTxnFee
+        merchant.integrationType = input.integrationType
+        merchant.adminName = input.adminName
+        merchant.adminEmail = input.adminEmail
+        merchant.adminPhone = input.adminPhone
+        merchant.fraudName = input.fraudName
+        merchant.fraudEmail = input.fraudEmail
+        merchant.fraudPhone = input.fraudPhone
         repository.save(merchant)
         return merchant
     }
